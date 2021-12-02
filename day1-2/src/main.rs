4ecb985ef589f35aea34ffe8a7e9a4d91b7abde5
Zachary Freed <zachary.freed@softwire.com>
2021-12-02 15:06:14 +0000

    Improved Day1-2 somewhat

--- day1-2/src/main.rs
+++ day1-2/src/main.rs
@@ -3,9 +3,8 @@ use std::io;
 use std::io::BufRead;
 
 fn main() -> Result<(), Box<dyn error::Error>> {
-    let mut prev_depth = None;
-    let mut num_increase = 0;
-    let depth_sums : Vec<_> = io::stdin().lock()
+    let depth_increases = io::stdin()
+        .lock()
         .lines()
         .collect::<Result<Vec<_>, _>>()?
         .iter()
@@ -13,16 +12,12 @@ fn main() -> Result<(), Box<dyn error::Error>> {
         .collect::<Result<Vec<_>, _>>()?
         .windows(3)
         .map(|window| window.iter().fold(0, |x, y| x + y))
-        .collect();
+        .fold((0, None), |(result, prev), current| match prev {
+            Some(prev) if current > prev => (result + 1, Some(current)),
+            _ => (result, Some(current)),
+        })
+        .0;
 
-    for depth in depth_sums {
-        if let Some(x) = prev_depth {
-            if depth > x {
-                num_increase += 1;
-            }
-        }
-        prev_depth = Some(depth);
-    }
-    println!("Increased depth {} times", num_increase);
+    println!("Increased depth {} times", depth_increases);
     Ok(())
 }
